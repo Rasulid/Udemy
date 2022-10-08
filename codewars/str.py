b23b985d5f844cee931e44ccd2bc09a919318014
Rasulabduvaitov <rasulabduvaitov@gmail.com>
2022-10-08 10:20:15 +0500

    Udemy couse

---
+++ codewars/str.py
@@ -0,0 +1,28 @@
+from pickletools import string1
+import string
+
+
+def solution(string):
+    for i in range(string[::-1]):
+      string = str(i)
+    
+
+
+# solution(string=input())
+  # return string[::-1]
+print(solution("world"))
+
+
+
+"""В Этом задаче я должен писать данное слово на оборот"""
+
+
+# a =int(input())
+# if a % 2 == 0:
+#   print("True")
+# else:
+#   print("Folse")
+
+
+# are_equal = 2==2
+# are_equal
---
+++ first/1.py
@@ -0,0 +1,28 @@
+"""from ast import Return
+
+
+num_1 =int(input())
+num_2 =int(input())
+#unct =input("")
+
+def function(oper):
+    if oper == "*":
+        print(num_1 * num_2)
+    elif oper == "+":
+        print(num_1 + num_2)
+    elif oper == "-":
+        print(num_1 - num_2)
+    elif oper == "%":
+        print(num_1 % num_2)
+    elif oper == "/":
+        print(num_1 / num_2)
+    else:
+        print("YOUR OPERATION IS DEFAULT")
+    return(function)
+
+function(oper=(input("""""#Chuuse the operation
+  #                      (+,-,*,/,%)""")))""
+                     #   """
+
+import calendar
+print(calendar.month(2022, 28))
